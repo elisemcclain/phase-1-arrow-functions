2d0a6877d415ba884d35e58752736a73cf8dad45
Elise McClain <celisemcclain@gmail.com>
2023-06-14 19:16:51 -0500

    arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(a, b) {
+    return a / b
+}
+console.log(2000 / 100)
+
+const square = (a) => a * a
+console.log(5)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
+console.log(7, 14)
